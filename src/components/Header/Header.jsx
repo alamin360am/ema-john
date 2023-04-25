5442580cb245b4035df905128efae0720ab5ea6e
alamin360am <alaminhimu0@gmail.com>
2023-04-25 14:21:14 +0600

    Header added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,20 @@
 // import React from 'react';
+import Logo from '../../images/Logo.svg';
+import './header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h1>header</h1>
-        </div>
+        <nav className='align-center'>
+            <div className="logo">
+                <img src={Logo} alt="Logo" />
+            </div>
+            <div className="nav-link">
+                <a href="">Order</a>
+                <a href="">Order Review</a>
+                <a href="">Manage Inventory</a>
+                <a href="">Login</a>
+            </div>
+        </nav>
     );
 };
 
